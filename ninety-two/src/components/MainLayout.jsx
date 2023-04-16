c20a897fd6b85f2bf3e95d231728dc26769fae45
Evans Wang <gaexp225125@gmail.com>
2023-04-16 14:04:43 +0800

    update mainLayout
    update  mainLayout

--- ninety-two/src/components/MainLayout.jsx
+++ ninety-two/src/components/MainLayout.jsx
@@ -3,20 +3,37 @@ import Box from "@mui/material/Box";
 import { Product } from "./Product";
 const styles = {
   mainContainer: {
-    height: "60vh",
+    height: "150vh",
     color: "red",
-    backgroundImage: "url('https://i.imgur.com/WjpmQ5f.jpeg')",
+    backgroundColor: "#FDF5E6",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     overflow: "hidden",
   },
+  landingImage: {
+    mt: 30,
+    width: "99vw",
+    borderRadius: "8px",
+  },
 };
-
+{
+  /* <Product /> */
+}
 export const MainLayout = () => {
   return (
-    <Box>
-      <Box sx={styles.mainContainer}>
-        <Product />
+    <Box sx={styles.mainContainer}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+        }}
+      >
+        <Box
+          component={"img"}
+          sx={styles.landingImage}
+          src={require("../images/landing_image.png")}
+        ></Box>
       </Box>
     </Box>
   );
