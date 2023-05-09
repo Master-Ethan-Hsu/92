98521122457690b88067b3314e670d208dd3e572
gaexp225120 <gaexp225125@gmail.com>
2023-05-09 23:00:40 +0800

    Revert "data"
    This reverts commit 04ef3855f167ac7fbb4b4fdd82f79dea21ade2c9.
    
    # Conflicts:
    #	ninety-two/src/components/Dessert.jsx
    #	ninety-two/src/components/Navbar.jsx

--- ninety-two/src/components/Navbar.jsx
+++ ninety-two/src/components/Navbar.jsx
@@ -1,29 +1,29 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import logo from '../images/logo.png';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useNavigate } from 'react-router-dom';
-import { useMediaQuery } from '@mui/material';
-import { useState } from 'react';
-import Drawer from '@mui/material/Drawer';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import logo from "../images/logo.png";
+import { useNavigate } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import Drawer from "@mui/material/Drawer";
 
 const styles = {
   NavbarStyle: {
-    background: '#c3b3a4',
-    position: 'sticky',
+    background: "#c3b3a4",
+    position: "sticky",
   },
   NavbarText: {
     fontSize: 15,
-    cursor: 'pointer',
-    mr: '1rem',
+    cursor: "pointer",
+    mr: "1rem",
     // "@media (max-width: 900px)": {
     //   fontSize: 6,
     // },
@@ -32,7 +32,7 @@ const styles = {
 
 export const Navbar = () => {
   const navigate = useNavigate();
-  const media900 = useMediaQuery('(max-width: 900px)');
+  const media900 = useMediaQuery("(max-width: 900px)");
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const toggleDrawer = () => () => {
@@ -40,9 +40,9 @@ export const Navbar = () => {
   };
 
   const list = () => (
-    <Box sx={{ width: 280 }} role='presentation' onClick={toggleDrawer()}>
+    <Box sx={{ width: 280 }} role="presentation" onClick={toggleDrawer()}>
       <List sx={{ pt: 3, pl: 2 }}>
-        {['麵包Bread', '咖啡Coffee', '關於我們About us', 'Q&A'].map((text) => (
+        {["麵包Bread", "咖啡Coffee", "關於我們About us", "Q&A"].map((text) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemText primary={text} />
@@ -57,18 +57,18 @@ export const Navbar = () => {
     <Box>
       {media900 ? (
         <AppBar sx={styles.NavbarStyle}>
-          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
             <IconButton
-              size='large'
-              edge='start'
-              color='inherit'
-              aria-label='menu'
-              sx={{ display: 'flex' }}
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ display: "flex" }}
             >
               <Box
-                component='img'
-                sx={{ width: '40px', height: '40px' }}
-                alt='The house from the offer.'
+                component="img"
+                sx={{ width: "40px", height: "40px" }}
+                alt="The house from the offer."
                 src={logo}
               />
             </IconButton>
@@ -76,12 +76,12 @@ export const Navbar = () => {
             <Box>
               <MenuIcon onClick={toggleDrawer()}></MenuIcon>
               <Drawer
-                anchor='right'
+                anchor="right"
                 open={isDrawerOpen}
                 onClose={toggleDrawer()}
                 PaperProps={{
                   sx: {
-                    backgroundColor: '#c3b3a4',
+                    backgroundColor: "#c3b3a4",
                   },
                 }}
               >
@@ -92,31 +92,31 @@ export const Navbar = () => {
         </AppBar>
       ) : (
         <AppBar sx={styles.NavbarStyle}>
-          <Toolbar sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Toolbar sx={{ display: "flex", flexDirection: "column" }}>
             <IconButton
-              size='large'
-              edge='start'
-              color='inherit'
-              aria-label='menu'
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
               sx={{ mr: 2 }}
             >
               <Box
-                component='img'
-                sx={{ width: '90px', height: '90px' }}
-                alt='The house from the offer.'
+                component="img"
+                sx={{ width: "90px", height: "90px" }}
+                alt="The house from the offer."
                 src={logo}
               />
             </IconButton>
             <Box
               sx={{
-                display: 'flex',
+                display: "flex",
                 mb: 2.5,
               }}
             >
               <Typography
                 sx={styles.NavbarText}
                 onClick={() => {
-                  navigate('/');
+                  navigate("/");
                 }}
               >
                 首頁 Home
@@ -124,7 +124,7 @@ export const Navbar = () => {
               <Typography
                 sx={styles.NavbarText}
                 onClick={() => {
-                  navigate('/dessert');
+                  navigate("/dessert");
                 }}
               >
                 甜點Dessert
