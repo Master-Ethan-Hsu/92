dd5b0187d12068e404dae86416ee8a269c779a47
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2023-05-14 15:54:32 +0800

    product update
    product update

--- ninety-two/src/components/Product.jsx
+++ ninety-two/src/components/Product.jsx
@@ -1,62 +1,59 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { Box, Divider } from "@mui/material";
-export const Product = () => {
+export const Product = (productData) => {
+  console.log(productData);
   return (
     <Box
       sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-evenly" }}
     >
-      {showComponent ? (
-        productData.map((product, key) => {
-          return (
-            <Box
-              sx={{
-                width: 300,
-                minHeight: 380,
-                my: 3,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-              }}
-              key={key}
-            >
-              <Box title={product.name} sx={{ py: 0 }} />
-              <Box sx={{ p: 0 }}>
-                <Box
-                  component="img"
-                  sx={{ minHeight: 300 }}
-                  width="100%"
-                  src={product.pic}
-                  alt="Paella dish"
-                />
-                <Typography variant="body2" color="text.secondary">
-                  {product.note}
-                </Typography>
-              </Box>
+      {productData.productData.map((product, key) => {
+        return (
+          <Box
+            sx={{
+              width: 300,
+              minHeight: 380,
+              my: 3,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+            key={key}
+          >
+            <Box sx={{ py: 0 }}>{product.name}</Box>
+            <Box sx={{ p: 0 }}>
+              <Box
+                component="img"
+                sx={{ maxHeight: 168, objectFit: "cover" }}
+                width="100%"
+                src={product.pic}
+                alt="Paella dish"
+              />
+              {/* <Typography variant="body2" color="text.secondary">
+                {product.note}
+              </Typography> */}
+            </Box>
 
-              <Divider />
-              {product.eating && (
-                <Typography
-                  variant="body2"
-                  color="text.secondary"
-                  sx={{ mt: 1, px: 1 }}
-                >
-                  {`食用方式： ${product.eating}`}
-                </Typography>
-              )}
+            <Divider />
+            {/* {product.eating && (
               <Typography
                 variant="body2"
                 color="text.secondary"
-                sx={{ mt: 1, px: 1, py: 0, display: "flex" }}
+                sx={{ mt: 1, px: 1 }}
               >
-                {`NT ${product.price}`}
+                {`食用方式： ${product.eating}`}
               </Typography>
-            </Box>
-          );
-        })
-      ) : (
-        <Typography>no data</Typography>
-      )}
+            )} */}
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ mt: 1, px: 1, py: 0, display: "flex" }}
+            >
+              {`NT ${product.price}`}
+            </Typography>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
