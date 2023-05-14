1abfc6ab7e943d39d8caa1bc87502b7776aeb0ac
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2023-05-14 15:54:19 +0800

    component update
    component update

--- ninety-two/src/components/Dessert.jsx
+++ ninety-two/src/components/Dessert.jsx
@@ -1,40 +1,20 @@
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import { getProductData } from "../api/NTApi";
 import { useState, useEffect } from "react";
-import { Box, Divider } from "@mui/material";
+import Box from "@mui/material/Box";
+import Product from "./Product";
+import { usePrdouctCate } from "./../hook/usePrdouctCate";
 
 export const Dessert = () => {
-  const [productData, setProductData] = useState();
-
-  const getData = async () => {
-    const productData = await getProductData();
-    try {
-      if (productData) setProductData(productData);
-    } catch {
-      throw new Error("can't get product data !");
-    }
-  };
+  const [dessertData, setDessertData] = useState();
+  const dessert = usePrdouctCate("coffee");
 
   useEffect(() => {
-    getData();
-    console.log("call api");
+    dessert.then((data) => {
+      console.log(data);
+      setDessertData(data);
+    });
   }, []);
 
-  useEffect(() => {
-    console.log("getting data");
-    console.log(productData);
-    if (productData) {
-      setShowComponent(true);
-      console.log(productData);
-      console.log(
-        productData.map((product) => {
-          // console.log(product.id);
-          console.log(product.name);
-          // console.log(product.price);
-          // console.log(product.pic);
-        })
-      );
-    }
-  }, [productData]);
+  return (
+    <Box>{dessertData ? <Product productData={dessertData} /> : null}</Box>
+  );
 };
