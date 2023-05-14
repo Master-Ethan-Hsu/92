92e1986923a49f9b81d7de15360378a04f15275c
gaexp225120 <37056487+gaexp225120@users.noreply.github.com>
2023-05-14 15:55:13 +0800

    router update
    router update

--- ninety-two/src/App.js
+++ ninety-two/src/App.js
@@ -2,19 +2,25 @@ import { MainLayout } from "./components/MainLayout";
 import { Dessert } from "./components/Dessert";
 import { Navbar } from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ProductContext } from "./reactContext/useProduct";
+import { UIProvider } from "./hook/useContext";
+import { Bread } from "./components/Bread";
+import { Coffee } from "./components/Coffee";
 
 function App() {
   return (
     <BrowserRouter basename="/">
-      <ProductContext.Consumer>
+      <UIProvider>
         <Navbar />
         <Routes>
           <Route path="/" element={<MainLayout />} />
           <Route path="dessert" element={<Dessert />} />
+          <Route path="bread" element={<Bread />} />
+          <Route path="coffee" element={<Coffee />} />
+          <Route path="about" element={<Bread />} />
+          <Route path="qa" element={<Bread />} />
           {/* <Route path="page2" element={<Page2 />} /> */}
         </Routes>
-      </ProductContext.Consumer>
+      </UIProvider>
     </BrowserRouter>
   );
 }
